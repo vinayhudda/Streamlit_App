255426135dc9510569140da2e3d690deeac7f2b7
vinayhudda <huddavinay4@gmail.com>
2024-11-10 15:22:03 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import time
 # Set the app title 
 st.title('My First Streamlit App') 
 # Add a welcome message 
@@ -7,3 +8,8 @@ st.write('Welcome to my Streamlit app!')
 user_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
 # Display the customized message 
 st.write('Customized Message:', user_input)
+
+# Set a timer to shut down the app after 1 minute
+time.sleep(10)  # 60 seconds = 1 minute
+
+st.stop()
